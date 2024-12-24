e6b55d57365ed7cbb5407d6a5496bc3aa3cdf10f
Zikebuz <www.zik@yahoo.com>
2024-12-24 15:57:52 +0100

    Update to the whole portfolio
    24/12/2024

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logo from "./logo.png";
+import computertech from "./computertech.png";
+import creator from "./creator.png";
+import frontend from "./frontend.png";
+import graphicdesigner from "./graphicdesigner.png";
+import github from "./github.png";
+import vercel from "./vercel.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import microsoft from "./tech/microsoft.png";
+import adobe from "./tech/adobe.png";
+import javascript from "./tech/javascript.png";
+
+import coppers from "./company/coppers.png";
+// import solutec from "./company/solutec.png";
+import teknowledge from "./company/teknowledge.png";
+import zymek from "./company/zymek.png";
+import jswithzikebuz from "./company/jswithzikebuz.png"
+
+import newshub from "./newshub.png";
+import minicom from "./minicom.png";
+import coinradar from "./coinradar.jpg";
+
+export {
+  logo,
+  frontend,
+  creator,
+  computertech,
+  graphicdesigner,
+  github,
+  vercel,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  microsoft,
+  adobe,
+  coppers,
+  teknowledge,
+  zymek,
+  jswithzikebuz,
+  minicom,
+  newshub,
+  coinradar,
+};
